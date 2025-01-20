c8bea6ab22f3c4b9ea58a99ced5933f86ffa6d6b
nahom <nahomasnake12@gmail.com>
2025-01-20 21:05:37 +0300

    margin changed on message

--- oc-content/themes/epsilon/item.php
+++ oc-content/themes/epsilon/item.php
@@ -4,16 +4,14 @@
   <?php osc_current_web_theme_path('head.php'); ?>
   <link rel="stylesheet" media="print" href="<?php echo osc_current_web_theme_url('css/print.css?v=' . date('YmdHis')); ?>">
   <style>
-    /* @media screen and (max-width: 767px) {
+    @media screen and (max-width: 767px) {
     body .oc-chat.oc-closed {
-        left: auto; /* Reset left positioning */
-        right: 10px; /* Move to the right */
-        bottom: 55px; /* Adjust as needed */
+        bottom: 55px !important; /* Adjust as needed */
         width: 46px;
         height: 46px;
         min-height: 46px;
     }
-} */
+}
 /* 
       .oc-chat-button {
         margin-right: 0 !important;
