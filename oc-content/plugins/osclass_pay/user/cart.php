14108dfa021ff400f6437e646baa444686528be2
nahom <nahomasnake12@gmail.com>
2025-04-16 22:03:01 +0300

    fixed confirmation

--- oc-content/plugins/osclass_pay/user/cart.php
+++ oc-content/plugins/osclass_pay/user/cart.php
@@ -28,7 +28,6 @@
     exit;
   }
 
-
   // PROCESS VOUCHER
   if(Params::getParam('ospAction') == 'voucher') {
     if(Params::getParam('voucher') <> '') {
@@ -38,24 +37,18 @@
         osc_add_flash_error_message(sprintf(__('Voucher %s has not been added into cart: %s', 'osclass_pay'), '<u>' . Params::getParam('voucher') . '</u>', $voucher_valid['message']));
         osp_redirect(osc_route_url('osp-cart'));
         exit;
-
       } else {
         $voucher = ModelOSP::newInstance()->getVoucherByCode(Params::getParam('voucher'));
-        osp_cart_update($user_id, OSP_TYPE_VOUCHER . 'x1x' . $voucher['pk_i_id']);          // If there is voucher in cart, it will be removed & replaced
-
+        osp_cart_update($user_id, OSP_TYPE_VOUCHER . 'x1x' . $voucher['pk_i_id']);
         osc_add_flash_ok_message(sprintf(__('Voucher %s has been added into cart: %s', 'osclass_pay'), '<u>' . Params::getParam('voucher') . '</u>', $voucher_valid['message']));
         osp_redirect(osc_route_url('osp-cart'));
         exit;
-
       }
     }
   }
 
-
   $cart = osp_cart_content($user_id);
 
-
- 
   // Check if we have still some shippings in cart
   $has_shipping = false;
   foreach($cart as $c) {
@@ -65,28 +58,16 @@
     }
   }
 
-
-  // foreach($cart as $c) {
-    // if($c[1] == OSP_TYPE_SHIPPING) {
-      // $has_shipping = true;
-      // break;
-    // }
-  // }
-    
-  //print_r($req_shippings); 
-
-
   if($remove <> '') {
     osp_cart_remove($user_id, $remove);
     osc_add_flash_ok_message(__('Product removed from cart', 'osclass_pay'));
     osp_redirect(osc_route_url('osp-cart'));
     exit;
   }
-  
+
   $cart_string = ModelOSP::newInstance()->getCart($user_id);
   $cart = osp_cart_content($user_id);
-  
-  //print_r($cart);
+
 ?>
 
 <div class="osp-body osp-body-cart">
@@ -106,14 +87,14 @@
       <div class="osp-cart-col prod"><?php _e('Product', 'osclass_pay'); ?></div>
       <div class="osp-cart-col qty"><?php _e('Qty', 'osclass_pay'); ?></div>
       <div class="osp-cart-col pric"><?php _e('Total Price', 'osclass_pay'); ?></div>
-      <div class="osp-cart-col delt">&nbsp;</div>
+      <div class="osp-cart-col delt"> </div>
     </div>
-
 
     <?php 
       $i = 1; 
       $total = 0; 
       $count = 0; 
+      $product_names = array(); // Initialize array to collect product names
     ?>
 
     <?php if(count($cart) > 0) { ?>
@@ -130,49 +111,45 @@
             
             if($c[1] == OSP_TYPE_BOOKING) {
               $reservation = ModelOSP::newInstance()->getBooking(@$c[3]);
-
               if($reservation === false || $reservation['b_paid'] == 1) {
                 osp_cart_remove($user_id, $c);
                 osc_add_flash_warning_message(__('Booking/Reservation has already been paid or it was removed. Booking/Reservation has been removed from cart.', 'osclass_pay'));
                 osp_redirect(osc_route_url('osp-cart'));
-
               }
             }
             
             $total = $total + $price;
             $count = $count + ($c[1] == OSP_TYPE_VOUCHER ? 0 : $c[2]);
 
+            // Collect product names
+            if($c[1] == OSP_TYPE_VOUCHER) {
+              $product_names[] = osp_product_cart_name($c);
+            } else {
+              $product_names[] = $c[2] . 'x ' . osp_product_cart_name($c);
+            }
+
             if($c[1] == OSP_TYPE_PACK) {
               $contains_pack = true;
             }
-
             if($c[1] == OSP_TYPE_PRODUCT) {
               $contains_product = true;
             }
-
             if($c[1] == OSP_TYPE_OFFER) {
               $contains_offer = true;
             }
-            
-            if($c[1] == OSP_TYPE_AUCTION_PAY || $c[1] == OSP_TYPE_AUCTION_BUYOUT) { // || $c[1] == OSP_TYPE_AUCTION_FEE) {
+            if($c[1] == OSP_TYPE_AUCTION_PAY || $c[1] == OSP_TYPE_AUCTION_BUYOUT) {
               $contains_auction = true;
             }
-            
             if($c[1] == OSP_TYPE_AUCTION_BUYOUT) {
               $contains_auction_buyout = true;
             }
-            
-            
             if($c[1] == OSP_TYPE_BOOKING) {
               $contains_booking = true;
             }
-
             if($c[1] == OSP_TYPE_VOUCHER) {
               $voucher_valid = osp_check_voucher_id($c[3]);
-              
               if($voucher_valid['error'] <> 'OK') {
                 $voucher = ModelOSP::newInstance()->getVoucher($c[3]);
-
                 osp_cart_remove($user_id, implode('x', $c));
                 osc_add_flash_error_message(sprintf(__('Voucher %s removed from cart: %s', 'osclass_pay'), ($voucher['s_code'] <> '' ? $voucher['s_code'] : '----'), $voucher_valid['message']));
                 osp_redirect(osc_route_url('osp-cart'));
@@ -192,14 +169,12 @@
                   <?php $pack = ModelOSP::newInstance()->getPack($c[3]); ?>
                   <i class="fa fa-tag" style="color:<?php echo $pack['s_color']; ?>;"></i>
                 <?php } ?>
-
                 <?php echo osp_product_type_name($c[1]); ?>
               </span>
               <span class="p2"><?php echo osp_product_cart_name($c); ?></span>
             </div>
             <div class="osp-cart-col osp-has-tooltip qty <?php if(osp_quantity_editable($c[1])) { ?>osp-editable<?php } ?>" <?php if(osp_quantity_editable($c[1])) { ?>title="<?php echo osc_esc_html(__('Click to update quantity', 'osclass_pay')); ?>"<?php } ?>><span><?php echo ($c[1] == OSP_TYPE_VOUCHER ? '' : $c[2] . 'x'); ?></span></div>
             <div class="osp-cart-col pric <?php if($c[2] > 1) { ?>has-unit osp-has-tooltip<?php } ?>" <?php if($c[2] > 1) { ?>title="<?php echo osc_esc_html(__('Unit price', 'osclass_pay') . ': ' . osp_format_price($price/$c[2])); ?>"<?php } ?>><?php echo osp_format_price($price); ?></div>
-
             <div class="osp-cart-col delt">
               <?php if($c[1] == OSP_TYPE_SHIPPING) { ?>
                 <a href="#" onclick="return false;" class="osp-disabled osp-has-tooltip" title="<?php echo osc_esc_html(__('Shipping is required and cannot be removed. If you remove all products those require shipping, shipping will be removed from cart automatically', 'osclass_pay')); ?>"><i class="fa fa-trash-o"></i></a>
@@ -208,24 +183,22 @@
               <?php } ?>
             </div>
           </div>
-
           <?php $i++; ?>
         <?php } ?>
       </div>
 
       <?php 
-       if($total < 0) {
-         $total = 0;
-       }
+        if($total < 0) {
+          $total = 0;
+        }
       ?>
 
       <div class="osp-cart-row osp-cart-total">
-        <div class="osp-cart-col code">&nbsp;</div>
+        <div class="osp-cart-col code"> </div>
         <div class="osp-cart-col prod"><?php _e('Cart summary', 'osclass_pay'); ?></div>
         <div class="osp-cart-col qty"><?php echo $count; ?>x</div>
         <div class="osp-cart-col pric"><span><?php echo osp_format_price($total); ?></span></div>
       </div>
-
     <?php } else { ?>
       <div class="osp-cart-row osp-cart-empty">
         <i class="fa fa-warning"></i><span><?php _e('No products in your cart', 'osclass_pay'); ?></span>
@@ -239,10 +212,8 @@
     <div class="osp-voucher-inside">
       <form action="<?php echo osc_route_url('osp-cart'); ?>" method="POST" enctype="multipart/form-data">
         <input type="hidden" name="ospAction" value="voucher" />
-
         <label for="voucher"><?php _e('Add a promo code', 'osclass_pay'); ?></label>
         <input type="text" id="voucher" name="voucher" placeholder="<?php echo osc_esc_html(__('Add a promo code', 'osclass_pay')); ?>"/>
-
         <button type="submit"><?php _e('Apply', 'osclass_pay'); ?></button>
       </form>
     </div>
@@ -252,22 +223,16 @@
 <?php if($has_shipping === true) { ?>
   <?php 
     $address = trim(implode(', ', array_filter(array($user['s_country'], $user['s_region'], $user['s_city'], $user['s_city_area'], $user['s_zip'], $user['s_address']))));
-
     if($user['s_city'] == '' || $user['s_address'] == '') {
       $address_missing = true;
     }
-    
     if($user['s_city'] == '') { $shipping_error[] = __('City is missing', 'osclass_pay'); }
-    //if($user['s_zip'] == '') { $shipping_error[] = __('ZIP code is missing', 'osclass_pay'); }
     if($user['s_address'] == '') { $shipping_error[] = __('Address is missing', 'osclass_pay'); }
-    
     $phone = (trim($user['s_phone_mobile']) <> '' ? $user['s_phone_mobile'] : $user['s_phone_land']);
-    
     if($phone == '') {
       $phone_missing = true;
       $shipping_error[] = __('Phone number is missing, you must enter at least one phone number (mobile or land)', 'osclass_pay');
     }
-
     $shipping_error = trim(implode(', ', $shipping_error));
   ?>
   
@@ -315,10 +280,28 @@
   <div class="osp-pay-msg"><?php _e('You cannot pay auction buyout using bank transfer.', 'osclass_pay'); ?></div>
 <?php } ?>
 
-
-
 <?php osc_run_hook('osp_cart_before_pay_buttons'); ?>
 
+<?php
+  // Initialize array to collect clean product names
+  $product_names = array();
+
+  // Check if the cart has items
+  if (count($cart) > 0) {
+    foreach ($cart as $c) {
+      // Get the product name and strip HTML tags
+      $clean_name = strip_tags(osp_product_cart_name($c));
+      
+      // Add the clean name to the array (no quantity prefix)
+      $product_names[] = $clean_name;
+    }
+  }
+
+  // Combine product names into a single string
+  $product_names_str = implode(', ', $product_names);
+
+  // Use $product_names_str as the product_name parameter in payment buttons
+?>
 
 <?php if($total > 0 && !$phone_missing && !$address_missing) { ?>
   <ul class="osp-pay-button">
@@ -327,29 +310,76 @@
       $checksum = osp_create_checksum(@$user['pk_i_id'], @$user['pk_i_id'], @$user['s_email'], round($total, 2));
 
       if(osc_is_admin_user_logged_in() && osc_apply_filter('osp_cart_payment_admin', true) !== false) {
-        osp_admin_button(round($total, 2), sprintf(__('Pay %s cart items for %s by admin', 'osclass_pay'), $count, osp_format_price($total, 2)), '901x1x'.$user_id, array('user' => @$user['pk_i_id'], 'itemid' => @$user['pk_i_id'], 'email' => @$user['s_email'], 'amount' => round($total, 2)));
+        osp_admin_button(
+          round($total, 2), 
+          sprintf(__('Pay %s cart items for %s by admin', 'osclass_pay'), $count, osp_format_price($total, 2)), 
+          '901x1x'.$user_id, 
+          array(
+            'user' => @$user['pk_i_id'], 
+            'itemid' => @$user['pk_i_id'], 
+            'email' => @$user['s_email'], 
+            'amount' => round($total, 2),
+            'product_name' => $product_names_str // Add product_name parameter
+          )
+        );
       }
 
       if(!$contains_pack && !$contains_product && !$contains_offer && !$contains_booking && !$contains_auction && osc_apply_filter('osp_cart_payment_wallet', true) !== false) {
-        osp_wallet_button(round($total, 2), sprintf(__('Pay %s cart items for %s', 'osclass_pay'), $count, osp_format_price($total, 2)), '901x1x'.$user_id, array('user' => @$user['pk_i_id'], 'itemid' => @$user['pk_i_id'], 'email' => @$user['s_email'], 'amount' => round($total, 2)));
+        osp_wallet_button(
+          round($total, 2), 
+          sprintf(__('Pay %s cart items for %s', 'osclass_pay'), $count, osp_format_price($total, 2)), 
+          '901x1x'.$user_id, 
+          array(
+            'user' => @$user['pk_i_id'], 
+            'itemid' => @$user['pk_i_id'], 
+            'email' => @$user['s_email'], 
+            'amount' => round($total, 2),
+            'product_name' => $product_names_str // Add product_name parameter
+          )
+        );
       } 
 
       $base_url = osc_base_url();
       $image_path = $base_url . 'oc-content/themes/epsilon/images/';
       $images = [
-        $image_path . 'cbe_logo.webp',          // CBE
-        $image_path . 'awash_bank_logo.webp',   // Awash
-        $image_path . 'abyssinia_logo.png',     // Abyssinia
-        $image_path . 'telebirr_logo.png',      // telebirr
-        $image_path . 'm_pesa_logo.png'         // M-Pesa
+        $image_path . 'cbe_logo.webp',
+        $image_path . 'awash_bank_logo.webp',
+        $image_path . 'abyssinia_logo.png',
+        $image_path . 'telebirr_logo.png',
+        $image_path . 'm_pesa_logo.png'
       ];
 
       if(osp_param('bt_enabled') == 1 && osc_apply_filter('osp_cart_payment_transfer', true) !== false && !$contains_auction_buyout) {
-        osp_transfer_button(round($total, 2), sprintf(__('Pay %s cart items for %s', 'osclass_pay'), $count, osp_format_price($total, 2)), '901x1x'.$user_id, array('user' => @$user['pk_i_id'], 'itemid' => @$user['pk_i_id'], 'email' => @$user['s_email'], 'name' => @$user['s_name'], 'amount' => round($total, 2), 'checksum' => $checksum), $images);
+        osp_transfer_button(
+          round($total, 2), 
+          sprintf(__('Pay %s cart items for %s', 'osclass_pay'), $count, osp_format_price($total, 2)), 
+          '901x1x'.$user_id, 
+          array(
+            'user' => @$user['pk_i_id'], 
+            'itemid' => @$user['pk_i_id'], 
+            'email' => @$user['s_email'], 
+            'name' => @$user['s_name'], 
+            'amount' => round($total, 2), 
+            'checksum' => $checksum,
+            'product_name' => $product_names_str // Add product_name parameter
+          ), 
+          $images
+        );
       }
 
       if(osc_apply_filter('osp_cart_payment_money', true) !== false) {
-        osp_buttons(round($total, 2), sprintf(__('Pay %s cart items for %s', 'osclass_pay'), $count, osp_format_price($total, 2)), '901x1x'.$user_id, array('user' => @$user['pk_i_id'], 'itemid' => @$user['pk_i_id'], 'email' => @$user['s_email'], 'amount' => round($total, 2)));
+        osp_buttons(
+          round($total, 2), 
+          sprintf(__('Pay %s cart items for %s', 'osclass_pay'), $count, osp_format_price($total, 2)), 
+          '901x1x'.$user_id, 
+          array(
+            'user' => @$user['pk_i_id'], 
+            'itemid' => @$user['pk_i_id'], 
+            'email' => @$user['s_email'], 
+            'amount' => round($total, 2),
+            'product_name' => $product_names_str // Add product_name parameter
+          )
+        );
       }
     ?>
   </ul>
@@ -358,9 +388,21 @@
 <?php } else if($total <= 0 && $count > 0) { ?>
   <ul class="osp-pay-button">
     <label><?php _e('Click on button to complete order', 'osclass_pay'); ?></label>
-    <?php osp_wallet_button(round($total, 2), sprintf(__('Free checkout for %s cart items', 'osclass_pay'), $count), '901x1x'.$user_id, array('user' => @$user['pk_i_id'], 'itemid' => @$user['pk_i_id'], 'email' => @$user['s_email'], 'amount' => round($total, 2))); ?>
+    <?php 
+      osp_wallet_button(
+        round($total, 2), 
+        sprintf(__('Free checkout for %s cart items', 'osclass_pay'), $count), 
+        '901x1x'.$user_id, 
+        array(
+          'user' => @$user['pk_i_id'], 
+          'itemid' => @$user['pk_i_id'], 
+          'email' => @$user['s_email'], 
+          'amount' => round($total, 2),
+          'product_name' => $product_names_str // Add product_name parameter
+        )
+      ); 
+    ?>
   </ul>
 <?php } ?>
 
 <?php osc_run_hook('osp_cart_after_pay_buttons'); ?>
-
