bf8857e3679d50ecc900abf490ade407fe7d481b
nahom <nahomasnake12@gmail.com>
2025-04-26 00:55:41 +0300

    removed quirk form item post

--- oc-content/themes/epsilon/item-post.php
+++ oc-content/themes/epsilon/item-post.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" dir="<?php echo eps_language_dir(); ?>" lang="<?php echo str_replace('_', '-', osc_current_user_locale()); ?>">
 
 <head>
